f8de53e3d9c7443e4ece865a8e0c45c6f245a033
RuchitPatel11 <72643449+RuchitPatel11@users.noreply.github.com>
2021-06-16 18:19:45 +0530

    Add files via upload

---
+++ DAY-6/Studentrecord/Themepart/Sidebar.php
@@ -0,0 +1,28 @@
+<div id="sidebar">
+			<ul>
+				
+				<li>
+					<h2>Head Office</h2>
+					<p>Delhi</p>
+				</li>
+				<li>
+					<h2>Services</h2>
+					<ul>
+						<li><a href="#">Buildings</a></li>
+						<li><a href="#">Infrastructure</a></li>
+						<li><a href="#">Industrial</a></li>
+                                                <li><a href="#">Power</a></li>
+						
+					</ul>
+				</li>
+				<li>
+					<h2>Working Sites</h2>
+					<ul>
+						<li><a href="#">Mumbai</a></li>
+						<li><a href="#">Hyderabad</a></li>
+						<li><a href="#">Indore</a></li>
+						
+					</ul>
+				</li>
+			</ul>
+		</div>
